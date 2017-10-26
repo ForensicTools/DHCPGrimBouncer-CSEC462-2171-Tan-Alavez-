9d922a8ee180adbe9432b99fccb5a22618a31dd1
bt4168 <bt4168@rit.edu>
2017-10-26 19:34:30 -0400

    Added to grimb.py

--- grimb.py
+++ grimb.py
@@ -9,3 +9,17 @@
 ### authors: Benny Tan bt4168, Jhony Alavez jia2707
 ###
 ########################################################################
+
+import sys
+import os
+import re
+
+readfile = open("test.webtest", 'r')
+
+for line in readfile:
+    lst = line.split("<Request", "</Request>")
+##    if(re.findall(r'Upgrade-Insecure-Requests', line)):
+##        print(lst)
+    print(lst)
+
+readfile.close()
