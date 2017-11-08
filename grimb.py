c1da6630b847f99f59722cb81ff362508365fbf9
bt4168 <bt4168@rit.edu>
2017-11-07 20:58:47 -0500

    Begun OpenSSL implementation and d3 visualization

--- grimb.py
+++ grimb.py
@@ -16,6 +16,7 @@ import re
 import platform
 import time
 import glob
+import csv
 
 ##readfile = open("test.webtest", 'r')
 ##
@@ -43,29 +44,51 @@ def bhv():
     URL_folder = r'C:\GrimBouncer\Browsing_History'         # path folder to store URL csv files
     if not os.path.exists(URL_folder):                      # check to see if folder already exists
         os.makedirs(URL_folder)                             # create the folder
-    
+# uncomment this to make a bhv csv
     #bhv_command = r'C:\browsinghistoryview-x64\BrowsingHistoryView.exe /scomma C:\GrimBouncer\Browsing_History'
     #os.system(bhv_command + "\\" + str(time.strftime("%Y-%m-%d_T%H%M%S"))  + ".csv")    # csv file generated
  
-
-def get_URLs(filename):
+############
+# function:     gen_CSV
+#
+# Description:  Generates a new CSV containing information from bhv
+#               and OpenSSL output
+############
+def gen_csv(filename):
     # delimit by commas
     # grab first element(URL) and third element(time and date visited)
     # use dict to store?
-    f = open(r'C:\GrimBouncer\Browsing_History\\' + filename, 'r')
+##    f = open(r'C:\GrimBouncer\Browsing_History\\' + filename, 'r')
 
-    url_lst = []
-    for line in f:
-        lst = line.split(",")
-        url = lst[0]
-        #print(url)
-        if(url[0:4] == "http"):
-            print(url)
-            
-    
+    certcsv_folder = r'C:\GrimBouncer\Certificate_CSVs'         # path folder to store URL csv files
+    if not os.path.exists(certcsv_folder):                      # check to see if folder already exists
+        os.makedirs(certcsv_folder)                             # create the folder
+    newfile = open(r'C:\GrimBouncer\Certificate_CSVs\\' + filename[:-4] + '_cert.csv', 'w')
+
+    with open(r'C:\GrimBouncer\Browsing_History\\' + filename, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        wr = csv.writer(newfile, quoting=csv.QUOTE_ALL)
+        #categories = ['objectName','URL','datetime','URL'
+
+        counter = 0
+        for row in reader:
+            url = row[0]
+            if(url[0:4] == "http"):
+##                for r in csv.reader(newfile, delimiter=','):
+##                    if(url == r[0]):
+##                        pass
+                cert_lst= []
+                cert_lst.append(row[0])     # URL
+                cert_lst.append(row[2])     # date and time
+                cert_lst.append(row[3])     # times visited
+
+                ossl = os.system(
+
+                wr.writerow(cert_lst)
     f.close()
+    newfile.close()
     
-###########
+############
 # function: main
 #
 # Description: Driver for program, grimb.
@@ -91,7 +114,7 @@ def main():
     print("-----------------------------------\n\n\n")
     filename = input("Enter browsing history log: ")
 
-    get_URLs(filename)
+    gen_csv(filename)
 
 
 main()
